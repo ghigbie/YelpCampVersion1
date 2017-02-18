ae18cf254f5310394132f0d5e244a38dfd1f19c1
ghigbie <gehigbie@gmail.com>
2017-02-18 04:20:32 +0000

    fixed cloud 9 port problme

--- app.js
+++ app.js
@@ -6,10 +6,10 @@ app.use(express.static("public"));
 app.set("view engine", "ejs");
 
 app.get("*", function(req, res){
-   res.render("views/notFound"); 
+   res.render("notFound"); 
 });
 
-app.listen(process.env.Port, process.env.IP, function(){
+app.listen(process.env.PORT, process.env.IP, function(){
     console.log("The server has started. Yay!!! Time to celebrate : )");
 });
 
