5286ba9f90dbfd6b6a27db49bee52211ee2ed18f
ghigbie <gehigbie@gmail.com>
2017-02-18 04:09:13 +0000

    set up server to listen

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
 var express = require("express");
 var app = express();
+var request = require("request");
+
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+
+app.listen(process.env.Port, process.env.IP, function(){
+    console.log("The server has started. Yay!!! Time to celebrate : )");
+});
 
