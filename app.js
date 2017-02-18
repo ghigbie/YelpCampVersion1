b8cd2632cbf6c2e7801e3e16b006cd39a006eb8c
ghigbie <gehigbie@gmail.com>
2017-02-18 04:17:53 +0000

    fixed header problem

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ var request = require("request");
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 
+app.get("*", function(req, res){
+   res.render("views/notFound"); 
+});
+
 app.listen(process.env.Port, process.env.IP, function(){
     console.log("The server has started. Yay!!! Time to celebrate : )");
 });
