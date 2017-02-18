b4994b1a8266cf971683b3ea960235e4615d3909
ghigbie <gehigbie@gmail.com>
2017-02-18 04:22:38 +0000

    fixed render problem

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ var request = require("request");
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 
+app.get("/", function(req, res){
+    res.render("home");
+});
+
 app.get("*", function(req, res){
    res.render("notFound"); 
 });
